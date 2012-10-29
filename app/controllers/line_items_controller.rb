23934c22536495b3f5b56cf1172ac47079701d7e
zjhui <jiahui.tar.gz@gmail.com>
2012-10-29 15:49:00 +0800

    finished to Task F: Iteration F2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -49,10 +49,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
 #        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.html { redirect_to(store_url) }
         # when "Add to Cart",redirect to the store index
         format.html { redirect_to(store_url) }
         format.json { render json: @line_item, status: :created, location: @line_item }
+        format.js
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
